d8cf911a29932f9479f9838752f0414d0a52d208
Mikie <kcmadhu081@gmail.com>
2023-12-22 21:56:51 +0545

    axios route change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import axios from "axios";
+import axios from "../../node_modules/axios";
+
 import Header from "./assets/Header.jsx";
 import Footer from "./assets/Footer.jsx";
 
@@ -11,6 +12,10 @@ axios.defaults.baseURL = "http://localhost:5001";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <Header />
+    <div className="min-h-[96vh]">
       <App/>
+    </div>
+    <Footer />
   </BrowserRouter>
 );
